929f28aba3a82d19134dc8af3e201541d3bb99bf
masterthepixel <masterthepixel@gmail.com>
2022-12-12 11:28:26 -0500

    Update index.jsx
    Header text overlap fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -202,7 +202,7 @@ export default function Home({ articles }) {
             </div>
           </div>
           <div className="p-4 lg:order-first lg:row-span-2">
-            <h1 className="filter drop-shadow-lg font-body tracking-tighter font-[600] bg-gradient-to-b from-gray-50 to-zinc-50 bg-clip-text text-transparent dark:text-amber-300 text-5xl md:text-6xl lg:text-center lg:text-7xl ">
+            <h1 className="filter drop-shadow-lg font-body tracking-tighter font-[600] bg-gradient-to-b from-gray-50 to-zinc-50 bg-clip-text text-transparent dark:text-amber-300 text-5xl md:text-6xl lg:text-center lg:text-7xl -mt-12 md:mt-4">
               Create Lasting Memories
             </h1>
             <div className="mt-6 lg:text-xl space-y-7 text-zinc-50 dark:text-zinc-100">
